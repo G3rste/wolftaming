b5777ca7adaf92103c9fd2715b170c76b844ba7c
G3rste <philippgerstenberg@web.de>
2022-05-28 19:33:48 +0200

    use more grep and less sed in release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-version=$(grep 'version' resources/modinfo.json | sed -r 's/\s*"version":\s*"(([0-9]\.*)*)",?/\1/')
-petai=$(grep 'petai' resources/modinfo.json | sed -r 's/\s*"petai":\s*"(([0-9]\.*)*)",?/\1/' | sed -r 's/[0-9]*$/+/')
+version=$(grep -o '"version":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
+petai=$(grep -o '"petai":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*' | sed -r 's/[0-9]*$/+/')
 releasefile='bin/wolftaming_v'$version'_petai_v'$petai'.zip'
 dotnet build -c release
 mv bin/wolftaming.zip $releasefile
