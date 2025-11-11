2034e420f3f4e701eb2daa3696e5dabff2a9c9b7
G3rste <philippgerstenberg@web.de>
2025-11-11 23:06:48 +0100

    add contributors reward

--- src/Wolftaming.cs
+++ src/Wolftaming.cs
@@ -1,4 +1,8 @@
-﻿using Vintagestory.API.Common;
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Xml;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
 using Vintagestory.GameContent;
 
 namespace Wolftaming
@@ -13,6 +17,20 @@ namespace Wolftaming
 
             AiTaskRegistry.Register<AiTaskPlayFetch>("playfetch");
             AiTaskRegistry.Register<AiTaskStayCloseToShepherd>("stayclosetoshepherd");
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Event.PlayerCreate += player => giveSecretDoggo(api, player);
+        }
+
+        private void giveSecretDoggo(ICoreServerAPI api, IServerPlayer player)
+        {
+            // Peachwolf updated the models -> Peachwolf gets his very own peach wolf
+            if (player.PlayerUID == "eJNd6SdFvvE028GvnRfJM2Yp")
+            {
+                player.InventoryManager.TryGiveItemstack(new ItemStack(api.World.GetItem(new AssetLocation("wolftaming:creature-dog-peach-female"))));
+            }
         }
     }
 }
