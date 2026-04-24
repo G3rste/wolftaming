699f5d38de693184708a29e9c3dc791b5a16cd08
Artalus <artalus-mail@yandex.ru>
2026-04-24 15:13:33 +0400

    fix paths in prepare scripts

--- prepare-debugging.ps1
+++ prepare-debugging.ps1
@@ -1,4 +1,4 @@
-Remove-Item "bin/vsdebug" -Force -Recurse; 
-New-Item -Path "bin/vsdebug" -Name "vsvillage" -ItemType "directory"; 
-Copy-Item -Path "resources/*" -Destination "bin/vsdebug/vsvillage" -Recurse;
-Copy-Item -Path "bin/Debug/net7.0/*" -Destination "bin/vsdebug/vsvillage";
+Remove-Item "bin/vsdebug" -Force -Recurse;
+New-Item -Path "bin/vsdebug" -Name "wolftaming" -ItemType "directory";
+Copy-Item -Path "resources/*" -Destination "bin/vsdebug/wolftaming" -Recurse;
+Copy-Item -Path "bin/Debug/net10.0/*" -Destination "bin/vsdebug/wolftaming";
